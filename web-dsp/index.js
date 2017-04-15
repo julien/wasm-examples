589c5c6ad812d98210cf0fb310a0ef29eb41126c
Julien C <jcastelain@gmail.com>
2017-04-15 19:14:36 +0200

    Move web-dsp stuff into it's own folder

---
+++ web-dsp/index.js
@@ -0,0 +1,40 @@
+
+window.addEventListener('load', () => {
+
+  const canvas = document.getElementById('c');
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+
+  const ctx = canvas.getContext('2d');
+
+  let webdsp = {};
+  let img = null;
+
+  const loadImage = src => new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = e => reject(e);
+      img.src = src;
+    });
+
+  Promise.all([
+    loadImage('assets/cat.jpg'),
+    loadWASM()
+  ]).then(data => {
+
+    img = data[0];
+    webdsp = data[1];
+
+    const x = (canvas.width - img.width) / 2;
+    const y = (canvas.height - img.height) / 2;
+    const w = img.width;
+    const h = img.height;
+
+    ctx.drawImage(img, x, y, w, h);
+    const pixels = ctx.getImageData(x, y, img.width, img.height);
+    pixels.data.set(webdsp.sobelFilter(pixels.data, img.width, img.height, true));
+    ctx.putImageData(pixels, x, y);
+  });
+
+
+}, false);
